ee3d185b97dbf6a15bc583a2facbca110f12afef
Praveen Gowda I V <praveen.vvstgy@gmail.com>
2017-03-25 14:48:16 -0700

    Get Facebook Data

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
+from facebook import *
+import json
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	graph = GraphAPI("EAAF18IdZCWz8BAGdIMMbLK8nmtP1oPYzaKUi6pHsQ9sgv4QWNflJmpOPziMZAdfOZAuwHfztfRE7nvG1iZAy7l4MgvscYCB1ZAiyzGANZCjBtuJa9CbMUH7Uwae6GcnLs3SyOvFOZC1UxhWpBdnfZAAdwl4nyYOus6UZD")
+	return json.dumps(graph.get('333039026750643/feed'))
+
+@app.route("/callback")
+def callback():
+	return ""
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
